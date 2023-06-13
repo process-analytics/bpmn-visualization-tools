9fa15dcbfcca1121a3cf9d5af7134e642e5e37cb
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-06-13 11:22:32 +0200

    feat(miwg): configure viewport and exported file name (#24)
    This lets generated PNG image with a convenient display of most screens.
    Also export files with the right names when there are several BPMN
    diagrams within the file.

--- bpmn-rendering-miwg-test-suite/scripts/generate-screenshots.ts
+++ bpmn-rendering-miwg-test-suite/scripts/generate-screenshots.ts
@@ -17,6 +17,16 @@
 import * as fs from 'node:fs';
 import {chromium} from 'playwright-chromium';
 
+type Configuration = {
+    /**
+     * Number of diagrams in the file that needs to be exported.
+     * Currently, only export the first one and postfix the screenshot file name with a number.
+     * It will change with https://github.com/process-analytics/bpmn-visualization-js/issues/729
+     **/
+    diagramsNumber?: number;
+    viewport?: { width: number, height: number }
+}
+
 
 // TODO lot of things are duplicated with the 'version-to-version' tool
 const outputDirectory = 'build/screenshots';
@@ -25,17 +35,31 @@ fs.mkdirSync(outputDirectory, {recursive: true});
 
 
 // list files from the public directory
-// const diagrams = fs.readdirSync('public')
-//     .filter(file => file.endsWith('.bpmn'))
-//     .map(file => file.substring(0, file.indexOf('.bpmn')));
-// TODO temp to configure the viewport for each file
-const diagrams = ['A.1.0', 'B.2.0', 'C.8.0'];
+const diagrams = fs.readdirSync('public')
+    .filter(file => file.endsWith('.bpmn'))
+    .map(file => file.substring(0, file.indexOf('.bpmn')));
+// Use this to check a dedicated diagram
+// const diagrams = ['B.2.0'];
+// const diagrams = ['A.1.0', 'B.2.0', 'C.4.0'];
 
 // configuration stores viewport
-// TODO we currently use an arbitrary load configuration for fit. See if we need to configure it for each file and diagram
-// TODO for files that contain several diagrams, need to list diagram index, dedicated viewport
-const configuration = new Map([
-    ['B.2.0', {viewport: { width: 2078, height: 1616 }}],
+const configuration = new Map<string, Configuration>([
+    ['A.1.0', {viewport: {width: 771, height: 111}}],
+    ['A.2.0', {viewport: {width: 810, height: 343}}],
+    ['A.3.0', {viewport: {width: 770, height: 440}}],
+    ['A.4.0', {viewport: {width: 1222, height: 807}}],
+    ['A.4.1', {viewport: {width: 1284, height: 1037}}],
+    ['B.1.0', {viewport: {width: 1103, height: 1011}}],
+    ['B.2.0', {viewport: {width: 1926, height: 1413}}],
+    // ['C.1.0', {}], // no need for config
+    // ['C.2.0', {}], // no need for config
+    // ['C.3.0', {}], // no need for config
+    ['C.4.0', {diagramsNumber: 4, viewport: {width: 2240, height: 800}}], // viewport only for the first diagram (other viewports will be configured later when we support the rendering of more diagrams)
+    ['C.5.0', {diagramsNumber: 2, viewport: {width: 3821, height: 984}}], // viewport only for the first diagram (other viewports will be configured later when we support the rendering of more diagrams)
+    ['C.6.0', {viewport: {width: 2081, height: 942}}],
+    // ['C.7.0', {}], // no need for config
+    ['C.8.0', {viewport: {width: 2158, height: 850}}],
+    ['C.8.1', {viewport: {width: 1920, height: 800}}],
 ]);
 
 const baseUrl = 'localhost:5173/index.html';
@@ -50,25 +74,26 @@ const defaultViewPort = { width: 1280, height: 720 };
     for (const fileName of diagrams) {
         console.info('Processing file', fileName);
 
-        const viewPort = configuration.get(fileName)?.viewport ?? defaultViewPort;
+        const diagramConfiguration = configuration.get(fileName);
+        const viewPort = diagramConfiguration?.viewport ?? defaultViewPort;
         console.info('viewport:', viewPort);
         await page.setViewportSize(viewPort);
 
         await page.goto(`${baseUrl}?bpmnFileName=${fileName}`);
 
-        // TODO wait for generated element instead of waiting for 1 second (risk of flaky generation, see https://playwright.dev/docs/api/class-page#pagewaitfortimeouttimeout)
-        // we do this in tests (for specific elements, this require data attribute that are not available in all versions or the attribute name changes from version to version
-        // we could at least check mxgraph initialization (svg node in the bpmn-container, but it may not have the same id in all pages)
-        // or check the existence of bpmn svg nodes (probably the easiest way as they will be present for all versions)
         console.info('Waiting for diagram rendering...');
-        // TODO the 2nd group is not empty (should also be checked in the other tool)
+        // TODO wait for generated element instead of waiting for 1 second (risk of flaky generation, see https://playwright.dev/docs/api/class-page#pagewaitfortimeouttimeout)
+        // we do this in bpmn-visualization tests
+        // we could at least check mxgraph initialization or check the existence of a specific BPMN SVG nodes by looking for its related data-bpmn-id
         await page.waitForTimeout(1000);
         console.info('Wait done');
 
         // https://playwright.dev/docs/screenshots
         // TODO ensure no fullPage, configure css to make fitCenter work or configure viewport for each file
         // TODO when several bpmn diagram in a file, the file name must be prefix by '.<number>'
-        await page.screenshot({path: `${outputDirectory}/${fileName}-import.png`, fullPage: true});
+        const screenshotFileNamePostfix = diagramConfiguration?.diagramsNumber ? '.1' : '';
+        await page.screenshot({path: `${outputDirectory}/${fileName}-import${screenshotFileNamePostfix}.png`, fullPage: true});
+        console.info(`Screenshot generated for ${fileName}`);
         // TODO decide if we use need to use 'locator'
         // await page.locator('#bpmn-container').screenshot({path: `${outputDirectory}/${fileName}-import-1-alternative.png`});
     }
