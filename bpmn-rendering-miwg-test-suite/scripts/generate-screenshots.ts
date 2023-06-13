e20fc8587f0fa24d9afb808c47403a171038f9c7
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-06-13 07:52:31 +0200

    more viewport config

--- bpmn-rendering-miwg-test-suite/scripts/generate-screenshots.ts
+++ bpmn-rendering-miwg-test-suite/scripts/generate-screenshots.ts
@@ -35,35 +35,31 @@ fs.mkdirSync(outputDirectory, {recursive: true});
 
 
 // list files from the public directory
-// const diagrams = fs.readdirSync('public')
-//     .filter(file => file.endsWith('.bpmn'))
-//     .map(file => file.substring(0, file.indexOf('.bpmn')));
-// TODO temp to configure the viewport for each file
-const diagrams = ['A.4.1'];
+const diagrams = fs.readdirSync('public')
+    .filter(file => file.endsWith('.bpmn'))
+    .map(file => file.substring(0, file.indexOf('.bpmn')));
+// Use this to check a dedicated diagram
+// const diagrams = ['C.8.1'];
 // const diagrams = ['A.1.0', 'B.2.0', 'C.4.0'];
 
 // configuration stores viewport
-// TODO we currently use an arbitrary load configuration for fit. See if we need to configure it for each file and diagram
-// TODO for files that contain several diagrams, need to list diagram index, dedicated viewport
 const configuration = new Map<string, Configuration>([
     ['A.1.0', {viewport: {width: 771, height: 111}}],
     ['A.2.0', {viewport: {width: 810, height: 343}}],
     ['A.3.0', {viewport: {width: 770, height: 440}}],
     ['A.4.0', {viewport: {width: 1222, height: 807}}],
     ['A.4.1', {viewport: {width: 1284, height: 1037}}],
-    // ['B.1.0', {viewport: {width: 2078, height: 1616}}],
-    ['B.2.0', {viewport: {width: 2078, height: 1616}}],  // TODO keep ratio but reduce size? Review ref dimension
-    // ['C.1.0', {viewport: {width: 771, height: 111}}],
-    // ['C.2.0', {viewport: {width: 771, height: 111}}],
-    // ['C.3.0', {viewport: {width: 771, height: 111}}],
-    // viewport only for the first diagram (other viewports will be configured later when we support the rendering of more diagrams)
-    ['C.4.0', {diagramsNumber: 4}],
-    // viewport only for the first diagram (other viewports will be configured later when we support the rendering of more diagrams)
-    ['C.5.0', {diagramsNumber: 2}],
-    ['C.6.0', {viewport: {width: 2081, height: 942}}], // TODO keep ratio but reduce size?
-    // ['C.7.0', {viewport: {width: 771, height: 111}}],
-    ['C.8.0', {viewport: {width: 2158, height: 850}}],  // TODO keep ratio but reduce size?
-    // ['C.8.1', {viewport: {width: 771, height: 111}}],
+    ['B.1.0', {viewport: {width: 1103, height: 1011}}],
+    ['B.2.0', {viewport: {width: 1284, height: 942}}],
+    // ['C.1.0', {}], // no need for config
+    // ['C.2.0', {}], // no need for config
+    // ['C.3.0', {}], // no need for config
+    ['C.4.0', {diagramsNumber: 4}],  // no need for viewport
+    ['C.5.0', {diagramsNumber: 2, viewport: {width: 3821, height: 984}}],  // viewport only for the first diagram (other viewports will be configured later when we support the rendering of more diagrams)
+    ['C.6.0', {viewport: {width: 2081, height: 942}}],
+    // ['C.7.0', {}], // no need for config
+    ['C.8.0', {viewport: {width: 2158, height: 850}}],
+    ['C.8.1', {viewport: {width: 1920, height: 800}}],
 ]);
 
 const baseUrl = 'localhost:5173/index.html';
