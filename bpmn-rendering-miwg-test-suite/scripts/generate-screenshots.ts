52f021162c77fc4f1270d566962b81ba3145985b
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-06-19 16:09:29 +0200

    C.8.0

--- bpmn-rendering-miwg-test-suite/scripts/generate-screenshots.ts
+++ bpmn-rendering-miwg-test-suite/scripts/generate-screenshots.ts
@@ -43,7 +43,7 @@ fs.mkdirSync(outputDirectory, {recursive: true});
 //     .filter(file => file.endsWith('.bpmn'))
 //     .map(file => file.substring(0, file.indexOf('.bpmn')));
 // Use this to check a dedicated diagram
-const diagrams = ['C.8.1'];
+const diagrams = ['C.8.0'];
 // const diagrams = ['A.1.0', 'B.2.0', 'C.4.0'];
 
 // configuration stores viewport
@@ -100,7 +100,10 @@ const configuration = new Map<string, Configuration>([
     ['C.5.0', {diagramsNumber: 2, viewport: {width: 3821, height: 984}}], // viewport only for the first diagram (other viewports will be configured later when we support the rendering of more diagrams)
     ['C.6.0', {viewport: {width: 2081, height: 942}}],
     // ['C.7.0', {}], // no need for config
-    ['C.8.0', {viewport: {width: 2158, height: 850}}],
+    ['C.8.0', {
+        checkedBpmnElementId: '_a97c1a48-faba-447b-bfa6-7aa81a6fe0a0', // Notify Employee of Approval
+        viewport: {width: 2158, height: 850}
+    }],
     ['C.8.1', {
         checkedBpmnElementId: '_1a818a94-ba6f-413b-a7e8-6f8fd2a11e32', // Vacation Approval
         viewport: {width: 1920, height: 800}
